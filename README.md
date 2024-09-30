8473b97e5f601c9963781e97c142d7f6e479b419
Josh Robbs <josh@joshrobbs.com>
2024-09-30 10:57:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is code I threw together from ChatGPT to test a few things.
+
+It is untested. Use at your own risk.
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+Plugin Name: Disable WP Updates
+Plugin URI: http://wpmasterbuilder.com
+Description: A plugin to disable WordPress updates.
+Version: 1.0
+Author: Josh Robbs
+License: GPL2
+
+@package Disable_WP_Updates
+ */
+
+namespace Disable_WP_Updates;
+
+defined( 'ABSPATH' ) || die();
+
+add_filter( 'automatic_updater_disabled', '__return_true' );
+add_filter( 'pre_site_transient_update_core', '__return_null' );
+add_filter( 'pre_site_transient_update_plugins', '__return_null' );
+add_filter( 'pre_site_transient_update_themes', '__return_null' );
+add_filter( 'screen_options_show_update', '__return_false' );
+define( 'DISALLOW_FILE_MODS', true );
+add_action( 'admin_menu', 'restrict_plugin_theme_installation' );
+
+/**
+ * Remove the ability to install plugins and themes completely.
+ *
+ * @return void
+ */
+function restrict_plugin_theme_installation() {
+	remove_menu_page( 'plugins.php' ); // Remove the Plugins menu.
+	remove_submenu_page( 'themes.php', 'theme-editor.php' ); // Remove Theme Editor.
+	remove_menu_page( 'themes.php' ); // Remove the Themes menu.
+}
